b61dafe8af6eb840069254ec2c7361a9c4433deb
dladzina@alseco.kz <dladzina@alseco.kz>
2023-09-06 07:57:16 +0000

    Edited files:     /src/init.js

--- src/init.js
+++ src/init.js
@@ -20,6 +20,7 @@ init:
     
     bind("postProcess", function($context) {
         $context.session.lastState = $context.currentState;
+        $analytics.setComment($dialer.getTtsConfig().speed)
         //$context.session._lastState = $context.currentState;
         // log("**********" + toPrettyString($context.currentState));
         $context.session.AnswerCnt = $context.session.AnswerCnt || 0;
